8412c8ef7a905b78bc09a5604ace047e8e51d8fa
github username <rdg.brg@gmail.com>
2021-01-06 17:18:06 -0500

    Set movieResults state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Nominations from './components/nominations/Nominations';
 import SearchBar from './components/searchBar/SearchBar';
 import SearchResults from './components/searchResults/SearchResults';
@@ -7,11 +7,33 @@ import 'semantic-ui-css/semantic.min.css'
 import './styles/App.scss';
 
 function App() {
+
+  const [ movieResults, setMovieResults ] = useState([])
+
+  const fetchMovies = term => {
+    let searchTerm = term.split(" ").join("%20")
+
+    console.log("TERM -> ", searchTerm)
+
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=880b9cc1&s=${searchTerm}`)
+    .then(r => r.json())
+    .then(data => {
+      if (data.Response == "True") {
+        setMovieResults(data.Search)
+        // displayMovies(data.Search)
+      } else {
+        console.log("No movies found with that name")
+      }
+    })
+  }
+
+  console.log("movieResults", movieResults)
+
   return (
     <div>
       <div id="mainWrapper">
-        <SearchBar/>
-        <SearchResults/>
+        <SearchBar findMovies={fetchMovies} />
+        <SearchResults movieResults={movieResults}/>
         <Nominations/>
       </div>
     </div>
