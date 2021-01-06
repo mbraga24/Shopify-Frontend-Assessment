24a236305140c140faf3c91ffe770430cfe13c97
github username <rdg.brg@gmail.com>
2021-01-06 17:19:22 -0500

    Finish SearchResults structure

--- src/components/searchResults/SearchResults.js
+++ src/components/searchResults/SearchResults.js
@@ -1,25 +1,38 @@
 import React from "react";
-import { List, Button } from 'semantic-ui-react'
+import { List, Button, Image } from 'semantic-ui-react'
 
 import './Styles.scss';
 
-const SearchResults = () => {
-  return (
-    <div className="searchResults">
-      <List divided relaxed>
+const SearchResults = ({ movieResults = [] }) => {
+
+  console.log(movieResults)
+
+  const displayMovies = () => {
+    return movieResults.map(mv => (
+      <List key={mv.Title} divided relaxed>
         <List.Item>
           <div className="searchResults__alignItems">
             <List.Content className="searchResults__listContent">
-              <List.Icon name='film' size='large' verticalAlign='middle' />
-              <div>
-                <List.Header as='a'>Harry Potter</List.Header>
-                <List.Description as='a'>2006</List.Description>
+              <Image 
+                src={mv.Poster !== "N/A" ? mv.Poster : "./assets/movie_placeholder.png"} 
+                className="searchResults__poster"
+                size='tiny' 
+                verticalAlign='middle' />
+              <div className="searchResults__info">
+                <List.Header as='a'>{mv.Title}</List.Header>
+                <List.Description as='a'>Year: {mv.Year}</List.Description>
               </div>
             </List.Content>
-            <Button color='blue' size="tiny">Nominate</Button>
+            <Button color='blue' className="searchResults__Button" >Nominate</Button>
           </div>
         </List.Item>
       </List>
+    ))
+  }
+
+  return (
+    <div className="searchResults">
+      {displayMovies()}
     </div>
   )
 }
